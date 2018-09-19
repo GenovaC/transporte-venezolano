95332f55eb018e59eb4882b1744a4de3e2666af0
Génova Castillo <37961016+GenovaC@users.noreply.github.com>
2018-09-19 16:39:19 -0400

    Las cifras que muestra en estadisticas de usuario son calculadas. Son las reales que estan en el JSON

--- app/scripts/controllers/estadisticaUsuarioController.js
+++ app/scripts/controllers/estadisticaUsuarioController.js
@@ -18,9 +18,19 @@ app.controller('pieUserEstadisticController', ['$scope', '$http', function ($sco
     function cargarCifrasEstadisticas() {
 
         $http.get('http://localhost:3000/viajes_usuario')        
-        .then(function (r) {
-           //$scope.model = r.data;   
-            $scope.longitud = r.data.length;
+        .then(function (r) { 
+            $scope.datos = r.data;
+
+            $scope.totalViajes = $scope.datos.length;
+
+            $scope.totalEsperado = 0;
+            $scope.totalRecorrido = 0;
+
+            angular.forEach($scope.datos, function(result){
+                $scope.totalRecorrido += result.km;
+                $scope.totalEsperado += result.tiempoEspera;
+            });
+
         })
         .catch(function (r){
             console.log('Ha ocurrido un error:', r.status, r.data);
